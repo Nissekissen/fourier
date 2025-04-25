d46751765ac3f2f1915ef9f732db9a9f094d5a41
Nils Lindblad <nils.m.lindblad@gmail.com>
2025-04-25 11:37:35 +0200

    fixed test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use fft_lib::fft;
 
 fn main() {
-    let data = vec![0; 100];
+    let data: Vec<f64> = vec![0.0; 100];
     let result = fft(&data);
     println!("Result length: {}", result.real.len());
 }
