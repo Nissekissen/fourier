ee8d8b07590a62fd0c8a524c3aa915adeb732459
Nils Lindblad <nils.m.lindblad@gmail.com>
2025-04-28 11:36:33 +0200

    simple wav reader

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use fft_lib::fft;
+use fft_lib::{fft, get_frequencies};
+use audio_lib::wav_file_to_vec;
 
 fn main() {
-    let data: Vec<f64> = vec![0.0; 100];
-    let result = fft(&data);
-    println!("Result length: {}", result.real.len());
+    let audio = wav_file_to_vec("./audio/meow.wav").unwrap();
+    for chunk in &audio.chunked_data {
+        let fft_result = fft(chunk.clone().as_slice());
+        let frequencies = get_frequencies(&fft_result, audio.sample_rate);
+        println!("Frequencies: {:?}", frequencies);
+    }
 }
